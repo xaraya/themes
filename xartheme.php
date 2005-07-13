8e7c17436c2d25aceb54543b8c2a7ca830f1d1d6
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:17:37 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,22 @@
+<?php
+$themeinfo['name'] = 'ddf';
+$themeinfo['id'] = '739';
+$themeinfo['directory'] = 'ddf';
+$themeinfo['author'] = 'Dorothy Crawford';
+$themeinfo['homepage'] = 'http://www.alphabluedesign.com';
+$themeinfo['email'] = 'briahna25@xaraya.com';
+$themeinfo['description'] = 'DDF Theme';
+$themeinfo['contact_info'] = 'Dorothy Crawford, briahna25@xaraya.com';
+$themeinfo['publish_date'] = '05/18/2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+$themevars[] = array('name' => 'page_bg', 'value' => '#ffffff', 'description' => 'Page background color');
+$themevars[] = array('name' => 'block_bg', 'value' => '#C6E0FF', 'description' => 'Block background color');
+$themevars[] = array('name' => 'accent_bg', 'value' => '#CDD5F8', 'description' => 'Accent color');
+
+?>
