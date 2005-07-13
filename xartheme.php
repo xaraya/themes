7af954c4e0f1a2303379e9c48819d801467887da
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:18:06 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,21 @@
+<?php
+$themeinfo['name'] = 'diner-casual';
+$themeinfo['id'] = '998';
+$themeinfo['directory'] = 'diner-casual';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.dinerminor.com';
+$themeinfo['email'] = 'admin@dinerminor.com';
+$themeinfo['description'] = 'Theme for dinerminor';
+$themeinfo['contact_info'] = 'John Cox, admin@dinerminor.com';
+$themeinfo['publish_date'] = '03/04/03';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+$themevars[] = array('name' => 'page_bg', 'value' => '#ffffff', 'description' => 'Page background color');
+$themevars[] = array('name' => 'block_bg', 'value' => '#C6E0FF', 'description' => 'Block background color');
+$themevars[] = array('name' => 'accent_bg', 'value' => '#CDD5F8', 'description' => 'Accent color');
+?>
