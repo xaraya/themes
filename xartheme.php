70ea4067f25db5a373e7982ca4833016e6cf1102
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:21:48 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,18 @@
+<?php
+$themeinfo['name'] = 'wyome';
+$themeinfo['id'] = '803';
+$themeinfo['directory'] = 'wyome';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'niceguyeddie@xaraya.com';
+$themeinfo['description'] = 'Tableless port of Zen Garden Example';
+$themeinfo['contact_info'] = 'niceguyeddie@xaraya.com';
+$themeinfo['publish_date'] = '20th September 2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+?>
