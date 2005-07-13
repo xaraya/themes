92b1760791c9451879376e93a5d1f834ff2a64da
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:15:38 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,18 @@
+<?php
+$themeinfo['name'] = 'XBlue_Pro';
+$themeinfo['id'] = '31505';
+$themeinfo['directory'] = 'XBlue_Pro';
+$themeinfo['author'] = 'andyv';
+$themeinfo['homepage'] = 'http://xaraya.com/~andyv/showcases/';
+$themeinfo['email'] = 'andyv@xaraya.com';
+$themeinfo['description'] = 'clean pro grade css theme';
+$themeinfo['contact_info'] = 'andyv@xaraya.com';
+$themeinfo['publish_date'] = '15th July 2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.5';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+?>
