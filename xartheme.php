388a726a873c5d78a998a6f6b67fe9b5fa5bb2be
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:12:39 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This theme has two extra block groups 'bottomleft' and 'bottomright'.
+ */
+$themeinfo['name'] = 'NinthAveYellow';
+$themeinfo['id'] = '636363';
+$themeinfo['directory'] = 'NinthAveYellow';
+$themeinfo['author'] = 'Roger Keays';
+$themeinfo['homepage'] = 'http://www.ninthave.net';
+$themeinfo['email'] = 'r.keays@ninthave.net';
+$themeinfo['description'] = 'Spacious yellow theme';
+$themeinfo['contact_info'] = 'r.keays@ninthave.net';
+$themeinfo['publish_date'] = '10th October 2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
+
