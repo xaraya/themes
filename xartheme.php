5bfb30b576c764bd4a24a96be6e6231e92b29134
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:18:54 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'empty';
+$themeinfo['id'] = '8002';
+$themeinfo['directory'] = 'empty';
+$themeinfo['author'] = 'Rolf Meijer & Marcel van der Boom';
+$themeinfo['homepage'] = 'http://www.karu.nl';
+$themeinfo['email'] = 'rolf@karu.nl';
+$themeinfo['description'] = 'Empty theme, used as starting point';
+$themeinfo['contact_info'] = 'Rolf Meijer, rolf@karu.nl, www.karu.nl';
+$themeinfo['publish_date'] = '06/04/2004';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
