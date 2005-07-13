a2c56e8617e9ba4810b47c97c308a793fe9e8358
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:16:32 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,18 @@
+<?php
+$themeinfo['name'] = 'buddha';
+$themeinfo['id'] = '2';
+$themeinfo['directory'] = 'buddha';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'niceguyeddie@xaraya.com';
+$themeinfo['description'] = 'Tableless port of Buddah -- Zen Garden Example';
+$themeinfo['contact_info'] = 'niceguyeddie@xaraya.com';
+$themeinfo['publish_date'] = '7th December 2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+?>
