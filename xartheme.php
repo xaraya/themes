7d58bf5d3db16433c0426a19d95efef6fe004ba0
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:14:07 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] ='PostNukeBlue';
+$themeinfo['id'] ='9761';
+$themeinfo['directory'] ='PostNukeBlue';
+$themeinfo['author'] ='';
+$themeinfo['homepage'] ='';
+$themeinfo['email'] ='';
+$themeinfo['description'] ='PostNukeBlue';
+$themeinfo['contact_info'] ='';
+$themeinfo['publish_date'] ='1088800994';
+$themeinfo['license'] =' GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
