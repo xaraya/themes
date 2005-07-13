cb22c2b6942ad4692b3a0e5361c3971ada4a44cd
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:16:06 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * File: $Id$
+ * 
+ * Atom initialization functions
+ * 
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ * @subpackage atom theme
+ * @author John Cox
+ */
+$themeinfo['name'] = 'Atom';
+$themeinfo['id'] = '810';
+$themeinfo['directory'] = 'atom';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'niceguyeddie@xaraya.com';
+$themeinfo['description'] = 'Theme for Atom API for Xaraya content';
+$themeinfo['contact_info'] = 'John Cox, niceguyeddie@xaraya.com';
+$themeinfo['publish_date'] = '12/26/2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['class'] = '0';
+?>
