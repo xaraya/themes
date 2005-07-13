53579f1e94752dee44b9c9580664af2222f69625
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:19:57 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'headlite';
+$themeinfo['id'] = '198';
+$themeinfo['directory'] = 'headlite';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'niceguyeddie@xaraya.com';
+$themeinfo['description'] = 'Small tableless theme by Wyome Consulting';
+$themeinfo['contact_info'] = 'niceguyeddie@xaraya.com';
+$themeinfo['publish_date'] = '20th November 2004';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
