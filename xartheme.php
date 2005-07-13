585ed4e65a1314bd2f086896ceeb2c7e5e49bb9c
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:14:32 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,17 @@
+<?php
+$themeinfo['name'] = 'SeaBreeze';
+$themeinfo['id'] = '331';
+$themeinfo['directory'] = 'SeaBreeze';
+$themeinfo['author'] = 'Vanessa';
+$themeinfo['homepage'] = '';
+$themeinfo['email'] = 'vanessa@designs4nuke.com';
+$themeinfo['description'] = 'SeaBreeze Theme';
+$themeinfo['contact_info'] = 'vanessa@designs4nuke.com';
+$themeinfo['publish_date'] = '';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+?>
