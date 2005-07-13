2f4de51e0c4aa71ce6cb458a3e6ea105eb2e3351
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:22:43 +0000

    Initial commit

---
+++ style/index.php
@@ -0,0 +1,127 @@
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+	<!-- begin header block -->
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Generator" content="Xaraya :: 0.9.11" />
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<meta name="rating" content="General" />
+<!-- Linking for Mozilla and related browser site tab -->
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="http://www.yamka.com/index.php?theme=rss" />
+    <link rel="alternate" type="text/html" title="Print" href="http://www.yamka.com/index.php?theme=print" />
+
+<link rel="pingback" href="http://www.yamka.com/ws.php" />
+<link rel="Top" type="text/html" href="http://www.yamka.com/" />
+<link rel="Parent" type="text/html" href="http://www.yamka.com/" />
+<link rel="author" href="mailto:john.cox@wyome.com" />
+<link rel="contents" type="text/html" href="http://www.yamka.com/index.php?module=articles&amp;func=viewmap" />
+<!-- end header block -->	<link rel="icon" href="favicon.ico" />
+	<title>Yamka :: Welcome</title> 
+	<base href="http://www.yamka.com/index.php" />
+	<style type="text/css">@import url(http://www.yamka.com/themes/wyome_yamka/style/style.css);</style>
+		
+		</head>
+	<body onload="window.defaultStatus='Wyome: Custom Design and Development';" id="wyome">
+   <div id="description">
+       <h2>
+           Yamka offers e-commerce solutions built on the Xaraya Development Platform.
+       </h2>
+   </div>
+   <div id="topcontainer"></div>
+   <div id="container">
+       <div id="logo"></div>
+       <div id="summarycontainer">
+<div id="summary">
+   <h2>Welcome to Yamka</h2>
+   Yamka provides solutions for Xaraya.  Each download on Yamka is provided either by subscription or on a pay as you go basis.  All items released from Yamka follow the GNU/GPL or GNU/FDL license.  Subscriptions for Yamka are $50 a year and individual downloads vary.  Email support is provided for each download.
+</div><div class="login">
+    <h2>
+        Online    </h2>
+    <table style="width:100%;">
+        <tr>
+            <td colspan="4" style="height:5px;">
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="modules/roles/xarimages/ob_users.gif" alt="" style="width:16px; height:16px; vertical-align: middle;" />
+ Total users              </td>
+             <td>
+             </td>
+             <td style="text-align: center;" colspan="2">
+                 0            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="modules/roles/xarimages/ob_users_an.gif" alt="" style="width:16px; height:16px; vertical-align: middle;" />
+ Total guests              </td>
+             <td>
+             </td>
+             <td style="text-align: center;" colspan="2">
+                 1            </td>
+        </tr>
+   </table>
+        <p>
+ Welcome to our latest member,             <a href="http://www.yamka.com/index.php?module=roles&amp;func=display#&amp;uid=3">
+                 John Cox            </a>
+        </p>
+</div><div class="login">
+    <h2>
+        Login    </h2>
+    <form action="http://www.yamka.com/index.php?module=roles&amp;func=login" method="post">
+    <p>
+        <input type="hidden" name="redirecturl" id="returnurl4" value="http://www.yamka.com/index.php" />
+        <label for="uname4">
+           <strong>
+ Username:             </strong>
+        </label>
+        <input class="xar-fullwidth" type="text" name="uname" id="uname4" maxlength="64" size="10" />
+    </p>
+    <p>
+        <label for="pass4">
+           <strong>
+ Password:             </strong>
+        </label>
+        <input class="xar-fullwidth" type="password" name="pass" id="pass4" maxlength="64" size="10" />
+   </p>
+   <p>
+        <input type="submit" value=" Log in " />
+   </p>
+</form></div>       </div>
+       <div id="modulescontainer">
+           <div id="modulessummary">
+               <h2>Module Download Summary</h2>
+               Load the module space for the modules summary
+           </div>
+       </div>
+       <div id="themescontainer">
+           <div id="themessummary">
+               <h2>Theme Download Summary</h2>
+               Load the module space for the themes summary
+           </div>
+       </div>
+       <div id="docscontainer">
+           <div id="docssummary">
+               <h2>Documentation Summary</h2>
+               Load the module space for the docs summary
+           </div>
+       </div>
+       <div id="navcontainer">
+            <ul id="navlist">
+                <li><a href="http://www.yamka.com/index.php" id="current" title="Back to the main page">Home</a></li>
+                <li><a href="javascript:window.scrollBy(700,0)" title="Pro-Football Blog">Modules</a></li>
+                <li><a href="javascript:window.scrollBy(1100,0)" title="Resources">Themes</a></li>
+                <li><a href="javascript:window.scrollBy(1400,0)" title="Resources">Documentation</a></li>
+                <li><a href="http://www.yamka.com/index.php?module=sitecontact" title="Contact Us">Contact</a></li>
+           </ul>
+           <div id="modulespace">
+           </div>
+       </div>
+
+   </div>
+		</body>
+	</html>
---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'wyome_yamka';
+$themeinfo['id'] = '21474831';
+$themeinfo['directory'] = 'wyome_yamka';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'john.cox@wyome.com';
+$themeinfo['description'] = 'Tableless port of Zen Garden Example';
+$themeinfo['contact_info'] = 'john.cox@wyome.com';
+$themeinfo['publish_date'] = '13th Mar 2005';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['class'] = '2';
+?>
