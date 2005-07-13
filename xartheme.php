f185014c1c1bfacd2b7c13806f1d0e76c204d886
scmadmin <scmadmin@xaraya.com>
2005-07-13 15:22:57 +0000

    Initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'x_lady1';
+$themeinfo['id'] = '196901';
+$themeinfo['directory'] = 'x_lady1';
+$themeinfo['author'] = 'Marie Altobelli';
+$themeinfo['homepage'] = 'http://www.dndresources.com';
+$themeinfo['email'] = 'ladyofdragons@xaraya.com';
+$themeinfo['description'] = 'Theme based on loosely on a csszengarden theme, using css for layout';
+$themeinfo['contact_info'] = 'Doug Daulton, apakuni@xaraya.com';
+$themeinfo['publish_date'] = '11/09/2003';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
