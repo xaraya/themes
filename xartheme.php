9df590752fcfdf76aae7e62b5440fc6e0e021909
andyv <andyv@xaraya.com>
2005-07-27 04:12:32 +0000

    corrected dirname for proper recognition by the themes module and small corrections of default.xt

--- xartheme.php
+++ xartheme.php
@@ -1,18 +1,18 @@
 <?php
 $themeinfo['name'] = 'XBlue_Pro';
 $themeinfo['id'] = '31505';
-$themeinfo['directory'] = 'XBlue_Pro';
+$themeinfo['directory'] = 'xblue_pro';
 $themeinfo['author'] = 'andyv';
-$themeinfo['homepage'] = 'http://xaraya.com/~andyv/showcases/';
+$themeinfo['homepage'] = 'http://xaraya.com/~andyv/';
 $themeinfo['email'] = 'andyv@xaraya.com';
-$themeinfo['description'] = 'clean pro grade css theme';
+$themeinfo['description'] = 'a pro grade css theme (updated for xar 1.0-RC3+)';
 $themeinfo['contact_info'] = 'andyv@xaraya.com';
 $themeinfo['publish_date'] = '15th July 2003';
 $themeinfo['license'] = 'GPL';
-$themeinfo['version'] = '1.5';
+$themeinfo['version'] = '1.5.2';
 $themeinfo['xar_version'] = '1.0';
 $themeinfo['bl_version'] = '1.0';
 $themeinfo['class'] = '2';
 
-$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+/*$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');*/
 ?>
