673716e421c7707eb45e3152b317eea94fbc5c9d
niceguyeddie <niceguyeddie@xaraya.com>
2005-10-02 12:32:50 +0000

    initial cut of slideshow_manta

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'slideshow_manta';
+$themeinfo['id'] = '317';
+$themeinfo['directory'] = 'slideshow_manta';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'john.cox@wyome.com';
+$themeinfo['description'] = 'Turn Xaraya into a Power Point Presentation';
+$themeinfo['contact_info'] = 'john.cox@wyome.com';
+$themeinfo['publish_date'] = '14th Mar 2005';
+$themeinfo['license'] = 'CC';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['class'] = '2';
+?>
