2b76c060f66d56c2f085aeba09fe779e1d421fdf
niceguyeddie <niceguyeddie@xaraya.com>
2005-10-02 12:57:34 +0000

    initial cut of googlesitemap

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'googlesitemap';
+$themeinfo['id'] = '417';
+$themeinfo['directory'] = 'googlesitemap';
+$themeinfo['author'] = 'John Cox';
+$themeinfo['homepage'] = 'http://www.wyome.com';
+$themeinfo['email'] = 'john.cox@wyome.com';
+$themeinfo['description'] = 'XML Format for Google Sitemap';
+$themeinfo['contact_info'] = 'john.cox@wyome.com';
+$themeinfo['publish_date'] = '06/04/2005';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.-';
+$themeinfo['class'] = '0';
+?>
