d72ec17e821ec646d91a86d36bc09f02c58ed6d6
curtis <curtis@farnham.com>
2005-12-02 16:31:10 +0000

    initial upload of green-marinee theme

---
+++ xartheme.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Green Marinee - Port of a Wordpress theme by Ian Main
+*
+* @package unassigned
+* @copyright (C) 2002-2005 by The Digital Development Foundation
+* @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+* @link http://www.xaraya.com
+*
+* @subpackage green-marinee
+* @link http://xaraya.com/index.php/release/551.html
+* @author Curtis Farnham <curtis@farnham.com>
+*/
+$themeinfo['name'] = 'Green Marinee';
+$themeinfo['id'] = '551';
+$themeinfo['directory'] = 'green-marinee';
+$themeinfo['author'] = 'Curtis Farnham';
+$themeinfo['homepage'] = 'http://www.curtisfarnham.com';
+$themeinfo['email'] = 'curtis@farnham.com';
+$themeinfo['description'] = 'Port of Wordpress theme by Ian Main';
+$themeinfo['contact_info'] = 'curtis@farnham.com';
+$themeinfo['publish_date'] = '11/9/2005';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.5';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['class'] = '2';
+?>
