60de9ea4db5325e5cd3170e2674cdddbe8013dd4
marcel <marcel@hsdev.com>
2005-12-25 15:44:18 +0000

    Rough initial commit of ical theme 	* includes/icalsummary-common.xt: new file 	* modules/articles/includes/publinks.xt: new file 	* modules/articles/user-summary.xt: new file 	* modules/articles/user-view.xt: new file 	* pages/default.xt: new file 	* xartheme.php: new file

---
+++ xartheme.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * iCal theme, or something which resembles that, just an experiment
+ * 
+ * @copyright (C) 2006 by the Digital Development Foundation
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ * @package themes
+ * @subpackage ical
+ * @author Marcel van der Boom
+ */
+$themeinfo['name'] = 'ical';
+$themeinfo['id'] = '479399';
+$themeinfo['directory'] = 'ical';
+$themeinfo['author'] = 'Marcel van der Boom';
+$themeinfo['homepage'] = 'http://marcel.hsdev.com';
+$themeinfo['email'] = 'marcel@hsdev.com';
+$themeinfo['description'] = 'Theme to be able to subscribe to iCal calendars';
+$themeinfo['contact_info'] = 'Marcel van der Boom, marcel@hsdev.com';
+$themeinfo['publish_date'] = 'Xmas day, 2005';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '0';
+?>
