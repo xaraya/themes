65af26342b21a27cea97defcca1fe6277fa5d082
bigteeth <bigteeth@xaraya.com>
2006-04-27 01:47:56 +0000

    some notes

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'itheme';
+$themeinfo['id'] = '698';
+$themeinfo['directory'] = 'itheme';
+$themeinfo['author'] = 'Bigteeth';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = '';
+$themeinfo['description'] = 'just a theme';
+$themeinfo['contact_info'] = 'go to zwiggybo.com and send a PM, or email';
+$themeinfo['publish_date'] = '23 Apr 06';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
