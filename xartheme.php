c43485cf9f02d14027ed6bf18bd05f8ed37f6dbb
bigteeth <bigteeth@xaraya.com>
2006-05-06 00:07:20 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,21 @@
+<?php
+$themeinfo['name'] = 'zigmo3';
+$themeinfo['id'] = '128';
+$themeinfo['directory'] = 'zigmo3';
+$themeinfo['author'] = 'zwiggybo';
+$themeinfo['homepage'] = 'http://www.zwiggybo.com';
+$themeinfo['email'] = 'nospam@zwiggybo.com';
+$themeinfo['description'] = 'An industrial tabled theme';
+$themeinfo['contact_info'] = 'via the website';
+$themeinfo['publish_date'] = 'May 2006';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.1';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+
+$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
+$themevars[] = array('name' => 'page_bg', 'value' => '#ffffff', 'description' => 'Page background color');
+$themevars[] = array('name' => 'block_bg', 'value' => '#C6E0FF', 'description' => 'Block background color');
+$themevars[] = array('name' => 'accent_bg', 'value' => '#CDD5F8', 'description' => 'Accent color');
+?>
