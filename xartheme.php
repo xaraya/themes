61e51f1a97e897e4406500fcd596e549c37ecf6c
johnny <johnny@localmomentum.net>
2006-09-27 16:17:51 +0000

    first cupt at json output theme 	* : new dir 	* modules: new dir 	* modules/dynamicdata: new dir 	* modules/dynamicdata/objects: new dir 	* pages: new dir 	* index.html: new file 	* modules/dynamicdata/objects/showview.xt: new file 	* pages/default.xt: new file 	* pages/index.html: new file 	* xartheme.php: new file

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'JSON';
+$themeinfo['id'] = '525333';
+$themeinfo['directory'] = 'json';
+$themeinfo['author'] = 'Johnny Robeson';
+$themeinfo['homepage'] = 'http://www.xaraya.com';
+$themeinfo['email'] = 'johnny@localmomentum.net';
+$themeinfo['description'] = 'JSON Output Theme';
+$themeinfo['contact_info'] = 'Johnny Robeson, johnny@localmomentum.net';
+$themeinfo['publish_date'] = '12/31/9999';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '0.0.1';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['class'] = '0';
+?>
