dd7256ee5a38bcda29ef77bc5800c56555d98050
bigteeth <bigteeth@xaraya.com>
2006-11-18 06:10:34 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'simpleone';
+$themeinfo['id'] = '820';
+$themeinfo['directory'] = 'simpleone';
+$themeinfo['author'] = 'bigteeth';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = '';
+$themeinfo['description'] = 'A 2 column CSS positioned theme, to be used as starting point';
+$themeinfo['contact_info'] = 'zwiggybo.com';
+$themeinfo['publish_date'] = '18 Nov 2006';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
