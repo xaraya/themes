59746583032cc8bc1df73e18ea20f49468513052
bigteeth <bigteeth@xaraya.com>
2006-11-21 22:02:33 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'minnow';
+$themeinfo['id'] = '822';
+$themeinfo['directory'] = 'minnow';
+$themeinfo['author'] = 'bigteeth';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = '';
+$themeinfo['description'] = 'A 2 column CSS positioned theme';
+$themeinfo['contact_info'] = 'zwiggybo.com';
+$themeinfo['publish_date'] = '21 Nov 2006';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
