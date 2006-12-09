0101b746661f9004498370e443ffd09f62fab319
bigteeth <bigteeth@xaraya.com>
2006-12-09 04:30:38 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'orangeandgrey';
+$themeinfo['id'] = '432';
+$themeinfo['directory'] = 'orangeandgrey';
+$themeinfo['author'] = 'me';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = 'na';
+$themeinfo['description'] = '2 column starter theme';
+$themeinfo['contact_info'] = 'na';
+$themeinfo['publish_date'] = '8 dec 2006';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
