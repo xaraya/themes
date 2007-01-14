1c84eb3cc1141836ed7c85c345d7a01eb571edc9
bigteeth <bigteeth@xaraya.com>
2007-01-14 19:28:51 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'purplenurple';
+$themeinfo['id'] = '835';
+$themeinfo['directory'] = 'purplenurple';
+$themeinfo['author'] = 'bigteeth';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = '';
+$themeinfo['description'] = 'A simple 2 column CSS positioned theme';
+$themeinfo['contact_info'] = 'zwiggybo.com';
+$themeinfo['publish_date'] = '5 Jan 2007';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
