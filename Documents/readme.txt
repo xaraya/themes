099efda3f44f5ec3d41800cf92b2c08aec93a56e
bigteeth <bigteeth@xaraya.com>
2007-01-19 20:36:20 +0000

    initial commit

---
+++ Documents/readme.txt
@@ -0,0 +1,5 @@
+This theme is a port of a template I acquired from http://www.freecsstemplates.org/ named 'darkened'. It is a fixed-width theme with a black background, and a simple horizontal top-navigation bar.
+
+If included, please read the setup.txt file.
+
+Any questions, please visit http://zwiggybo.com/ and post them in the forums.
---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'darkened';
+$themeinfo['id'] = '841';
+$themeinfo['directory'] = 'darkened';
+$themeinfo['author'] = 'me';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = 'na';
+$themeinfo['description'] = 'a theme with a black background';
+$themeinfo['contact_info'] = 'na';
+$themeinfo['publish_date'] = '14 Jan 2007';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.1';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
