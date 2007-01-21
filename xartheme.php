f45f579cee8350479ec7ab903ed2167c7179e9de
bigteeth <bigteeth@xaraya.com>
2007-01-21 01:36:36 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'brownie';
+$themeinfo['id'] = '842';
+$themeinfo['directory'] = 'brownie';
+$themeinfo['author'] = 'me';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = 'na';
+$themeinfo['description'] = 'simple brown bordered theme with 992px fixed-width';
+$themeinfo['contact_info'] = 'na';
+$themeinfo['publish_date'] = '19 Jan 2007';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '0.9';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
