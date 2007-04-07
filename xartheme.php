c6a530bda64ff84e1c57c9fc10ec05a163c47ef7
primus <primus@zwiggybo.com>
2007-04-07 23:26:02 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'm83';
+$themeinfo['id'] = '885';
+$themeinfo['directory'] = 'm83';
+$themeinfo['author'] = 'bigteeth';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = '';
+$themeinfo['description'] = 'A theme';
+$themeinfo['contact_info'] = 'zwiggybo.com';
+$themeinfo['publish_date'] = '7 Apr 2007';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '0.1';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
