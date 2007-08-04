7bccb7cedac2b8361d9d4a8486a3b11066950479
marty.vance <marty.vance@gmail.com>
2007-08-04 07:40:57 +0000

    first workable version of tropical theme for xaraya.com

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'Xaraya Tropics';
+$themeinfo['id'] = '897';
+$themeinfo['directory'] = 'xartropics';
+$themeinfo['author'] = 'Marty Vance';
+$themeinfo['homepage'] = 'http://www.xaraya.com';
+$themeinfo['email'] = 'dracos@xaraya.com';
+$themeinfo['description'] = 'Trpoical theme for xaraya.com';
+$themeinfo['contact_info'] = 'Marty Vance, dracos@xaraya.com, www.xaraya.com';
+$themeinfo['publish_date'] = '04/22/2007';
+$themeinfo['license'] = 'GPL';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0';
+$themeinfo['bl_version'] = '1.0';
+$themeinfo['class'] = '2';
+?>
