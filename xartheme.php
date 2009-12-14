075280ec963e1eee41563f81565ddd7c99bb1845
marty.vance <marty.vance@gmail.com>
2009-12-14 19:54:12 +0000

    Update all version numbers in xartheme.php to 3 digits

--- xartheme.php
+++ xartheme.php
@@ -10,8 +10,8 @@ $themeinfo['contact_info'] = 'M. van der Boom, marcel@hsdev.com';
 $themeinfo['publish_date'] = '02/20/2003';
 $themeinfo['license'] = 'GPL';
 $themeinfo['version'] = '1.0.0';
-$themeinfo['xar_version'] = '1.0';
-$themeinfo['bl_version'] = '1.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
 $themeinfo['class'] = '2';
 
 $themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
