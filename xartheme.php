365e7cdf696067995902e4792976c6c6d4c72234
marty.vance <marty.vance@gmail.com>
2009-12-14 19:50:37 +0000

    Update all version numbers in xartheme.php to 3 digits

--- xartheme.php
+++ xartheme.php
@@ -10,7 +10,7 @@ $themeinfo['contact_info'] = 'zwiggybo.com';
 $themeinfo['publish_date'] = '18 Nov 2006';
 $themeinfo['license'] = 'GPL';
 $themeinfo['version'] = '1.3.0';
-$themeinfo['xar_version'] = '1.0';
-$themeinfo['bl_version'] = '1.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
 $themeinfo['class'] = '2';
 ?>
