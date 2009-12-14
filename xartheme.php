0fdd1e717ec9f8dfc9a5e871316deacf599c379b
marty.vance <marty.vance@gmail.com>
2009-12-14 19:53:48 +0000

    Update all version numbers in xartheme.php to 3 digits

--- xartheme.php
+++ xartheme.php
@@ -10,8 +10,8 @@ $themeinfo['contact_info'] = 'andyv@xaraya.com';
 $themeinfo['publish_date'] = '15th July 2003';
 $themeinfo['license'] = 'GPL';
 $themeinfo['version'] = '1.5.2';
-$themeinfo['xar_version'] = '1.0';
-$themeinfo['bl_version'] = '1.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
 $themeinfo['class'] = '2';
 
 /*$themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');*/
