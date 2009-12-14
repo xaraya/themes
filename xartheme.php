df175cfb0f9194662dd520c4c432ee5484f89c22
marty.vance <marty.vance@gmail.com>
2009-12-14 19:51:08 +0000

    Update all version numbers in xartheme.php to 3 digits

--- xartheme.php
+++ xartheme.php
@@ -11,8 +11,8 @@ the right-hand side of the Story Box displays a little off by a few pixels.';
 $themeinfo['contact_info'] ='Somara Sem';
 $themeinfo['publish_date'] ='1084735739';
 $themeinfo['license'] =' GPL';
-$themeinfo['version'] =' 1.0.0';
-$themeinfo['xar_version'] =' 1.0';
-$themeinfo['bl_version'] =' 1.0';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '1.0.0';
+$themeinfo['bl_version'] = '1.0.0';
 $themeinfo['class'] =' 2';
 ?>
