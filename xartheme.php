e380459359953a8ad4c8ae3b2992f9e9fadb2f70
primus <primus@zwiggybo.com>
2010-09-15 00:38:56 +0000

    initial commit

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'zulu';
+$themeinfo['id'] = '1042';
+$themeinfo['directory'] = 'zulu';
+$themeinfo['author'] = 'zwiggybo';
+$themeinfo['homepage'] = 'http://zwiggybo.com';
+$themeinfo['email'] = 'zwigcss@gmail.com';
+$themeinfo['description'] = 'A Simple Xaraya 2 Theme';
+$themeinfo['contact_info'] = 'zwiggybo';
+$themeinfo['publish_date'] = '09/14/2010';
+$themeinfo['license'] = 'Public Domain';
+$themeinfo['version'] = '0.3.0';
+$themeinfo['xar_version'] = '2.0';
+$themeinfo['bl_version'] = '2.0';
+$themeinfo['class'] = '2';
+?>
