3e927d49d8c3e4918540a013a60ec35d7184458c
crispcreations <crispcreations@googlemail.com>
2010-09-15 13:43:52 +0000

    Branch creation

---
+++ xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'BlueRay';
+$themeinfo['id'] = '189200';
+$themeinfo['directory'] = 'blueray';
+$themeinfo['author'] = 'Chris Powis';
+$themeinfo['homepage'] = 'http://crispcreations.co.uk';
+$themeinfo['email'] = 'crisp@crispcreations.co.uk';
+$themeinfo['description'] = 'Xaraya Jamaica BlueRay Theme';
+$themeinfo['contact_info'] = 'Chris Powis, crisp@crispcreations.co.uk, crispcreations.co.uk';
+$themeinfo['publish_date'] = '15-Sep-2010';
+$themeinfo['license'] = 'Public';
+$themeinfo['version'] = '2.0.0';
+$themeinfo['xar_version'] = '2.0';
+$themeinfo['bl_version'] = '2.0';
+$themeinfo['class'] = '2';
+?>
