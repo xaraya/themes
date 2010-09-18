3d7a84e0e6e6e694ebefcb3c289888312aacaab7
crispcreations <crispcreations@googlemail.com>
2010-09-18 16:48:00 +0000

    Create branch for Jamaica version of this theme

--- xartheme.php
+++ xartheme.php
@@ -10,7 +10,7 @@ $themeinfo['contact_info'] = 'Marty Vance, dracos@xaraya.com, www.xaraya.com';
 $themeinfo['publish_date'] = '04/22/2007';
 $themeinfo['license'] = 'GPL';
 $themeinfo['version'] = '1.0.0';
-$themeinfo['xar_version'] = '1.0';
+$themeinfo['xar_version'] = '2.0';
 $themeinfo['bl_version'] = '1.0';
 $themeinfo['class'] = '2';
 ?>
