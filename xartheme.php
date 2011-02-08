68a083abdfae9dde75b9f1ea246b69635d139d1a
ryan <ryan@webcommunicate.net>
2011-02-08 14:58:38 +0000

    version number change; change to 0.9 sitemap schema

--- xartheme.php
+++ xartheme.php
@@ -9,8 +9,8 @@ $themeinfo['description'] = 'XML Format for Google Sitemap';
 $themeinfo['contact_info'] = 'john.cox@wyome.com';
 $themeinfo['publish_date'] = '06/04/2005';
 $themeinfo['license'] = 'GPL';
-$themeinfo['version'] = '1.0.0';
-$themeinfo['xar_version'] = '1.0.0';
-$themeinfo['bl_version'] = '1.0.0';
+$themeinfo['version'] = '2.0.0';
+$themeinfo['xar_version'] = '2.0.0';
+$themeinfo['bl_version'] = '2.0.0';
 $themeinfo['class'] = '0';
 ?>
