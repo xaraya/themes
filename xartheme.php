b3c9933f0b173694f36a341a7db21cdaf52c3698
Marc Lutolf <mfl@netspan.ch>
2013-07-28 19:22:08 +0200

    First commit of this theme

---
+++ xartheme.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Twit Theme
+ *
+ * @package themes
+ * @subpackage twit theme
+ * @category Third Party Xaraya Theme
+ * @version 1.0.0
+ * @copyright 2013 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+$themeinfo['name'] = 'twit';
+$themeinfo['id'] = '30084';
+$themeinfo['directory'] = 'twit';
+$themeinfo['author'] = 'Twitter Bootstrap';
+$themeinfo['homepage'] = 'http://www.netspan.ch.';
+$themeinfo['email'] = 'mfl@netspan.ch';
+$themeinfo['description'] = 'Xaraya 2.x Theme based on Twitter Bootstrap';
+$themeinfo['contact_info'] = 'Marc Lutolf, mfl@netspan.ch';
+$themeinfo['publish_date'] = '01/07/2013';
+$themeinfo['license'] = 'Public Domain';
+$themeinfo['version'] = '1.0.0';
+$themeinfo['xar_version'] = '2.0';
+$themeinfo['bl_version'] = '2.0';
+$themeinfo['class'] = '2';
+?>
