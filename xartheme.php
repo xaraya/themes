275b2c8a7f5041e02894ac796e16d1466a4b63e3
Marc Lutolf <mfl@netspan.ch>
2016-04-18 10:56:40 +0200

    Minor updates
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- xartheme.php
+++ xartheme.php
@@ -1,16 +1,16 @@
 <?php
-$themeinfo['name'] = 'empty';
-$themeinfo['id'] = '8002';
-$themeinfo['directory'] = 'empty';
-$themeinfo['author'] = 'Rolf Meijer & Marcel van der Boom';
-$themeinfo['homepage'] = 'http://karu.nl';
-$themeinfo['email'] = 'rolf@karu.nl';
-$themeinfo['description'] = 'Empty theme, used as starting point';
+$themeinfo['name']         = 'empty';
+$themeinfo['id']           = '8002';
+$themeinfo['directory']    = 'empty';
+$themeinfo['author']       = 'Rolf Meijer & Marcel van der Boom';
+$themeinfo['homepage']     = 'http://karu.nl';
+$themeinfo['email']        = 'rolf@karu.nl';
+$themeinfo['description']  = 'Empty theme, used as starting point';
 $themeinfo['contact_info'] = 'Rolf Meijer, rolf@karu.nl, karu.nl';
 $themeinfo['publish_date'] = '05/05/2006';
-$themeinfo['license'] = 'Public Domain';
-$themeinfo['version'] = '2.0.0';
-$themeinfo['xar_version'] = '2.0';
-$themeinfo['bl_version'] = '2.0';
-$themeinfo['class'] = '2';
+$themeinfo['license']      = 'Public Domain';
+$themeinfo['version']      = '2.0.0';
+$themeinfo['xar_version']  = '2.0';
+$themeinfo['bl_version']   = '2.0';
+$themeinfo['class']        = '2';
 ?>
